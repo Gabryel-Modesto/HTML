2762b1e35ef5f4110aeba57cf711d7907a375d54
Gabryel-De-Souza <gabryelpsn@gmail.com>
2025-01-29 17:44:11 -0300

    js

---
+++ JavaScript/Estrutura-De-Programacao/js/script.js
@@ -0,0 +1,12 @@
+// 1 - Variáveis
+let name = "Gabryel";
+
+console.log(name);
+
+const idade = 20;
+
+// 2 - prompt
+
+const age = prompt('Digite sua idade:')
+
+console.log(`Você tem ${agr} anos`)
---
+++ JavaScript/TipoDeDados/js/script.js
@@ -0,0 +1,22 @@
+// 1 - Number
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -217);
+
+// 2 - Ops. Aritméticas
+
+console.log(2 + 4);
+console.log(10 - 9);
+console.log(8 * 2);
+console.log(10 / 10);
+
+
+console.log(5 + (4 * 2));
+
+// 3 - Special numbers
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(12 * "hdaosdh");
+console.log(NaN);
+
+// 4 - Strings
