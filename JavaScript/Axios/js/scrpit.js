d64388d90e0f6ad47991da927d0444ea303fe9eb
Gabryel-De-Souza <gabryelpsn@gmail.com>
2025-02-19 17:52:58 -0300

    Axios

---
+++ JavaScript/Axios/js/scrpit.js
@@ -0,0 +1,60 @@
+// 1 - installation
+console.log(axios);
+
+// 2 - first request
+const getData = async () => {
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users",
+      //  4 - definindo headers  
+      {
+        headers: {
+          "Content-Type": "application/json",
+          custom: "header",
+        }
+      }
+    );
+
+    console.log(response);
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getData();
+
+// 3 - Displaying data on the screen
+
+
+const container = document.querySelector("#user-container");
+
+const printData = async() => {
+  const data = await getData()
+
+  console.log(data)
+
+  data.forEach((user) => {
+
+    const div = document.createElement("div");
+
+    const nameElement = document.createElement("h2");
+
+    nameElement.textContent = user.name;
+
+    div.appendChild(nameElement);
+
+    const emailElement = document.createElement("p");
+
+    emailElement.textContent = user.email;
+
+    div.appendChild(emailElement);
+
+    container.appendChild(div);
+
+  })
+
+};
+
+printData();
